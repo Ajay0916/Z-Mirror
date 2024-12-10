b825f5c905ea4ff2a397adbeec7758f518c8ef3b
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-12-10 16:33:48 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -10,19 +10,19 @@ class _BotCommands:
         ]
         self.QbMirrorCommand = [
             f"qbmirror{CMD_SUFFIX}",
-            f"qbm{CMD_SUFFIX}",
+            f"qm{CMD_SUFFIX}",
         ]
         self.JdMirrorCommand = [
             f"jdmirror{CMD_SUFFIX}",
-            f"jdm{CMD_SUFFIX}",
+            f"jm{CMD_SUFFIX}",
         ]
         self.NzbMirrorCommand = [
             f"nzbmirror{CMD_SUFFIX}",
-            f"nzbm{CMD_SUFFIX}",
+            f"nm{CMD_SUFFIX}",
         ]
         self.YtdlCommand = [
             f"ytdlm{CMD_SUFFIX}",
-            f"ytm{CMD_SUFFIX}",
+            f"ym{CMD_SUFFIX}",
         ]
         self.LeechCommand = [
             f"leech{CMD_SUFFIX}",
@@ -30,19 +30,19 @@ class _BotCommands:
         ]
         self.QbLeechCommand = [
             f"qbleech{CMD_SUFFIX}",
-            f"qbl{CMD_SUFFIX}",
+            f"ql{CMD_SUFFIX}",
         ]
         self.JdLeechCommand = [
             f"jdleech{CMD_SUFFIX}",
-            f"jdl{CMD_SUFFIX}",
+            f"jl{CMD_SUFFIX}",
         ]
         self.NzbLeechCommand = [
             f"nzbleech{CMD_SUFFIX}",
-            f"nzbl{CMD_SUFFIX}",
+            f"nl{CMD_SUFFIX}",
         ]
         self.YtdlLeechCommand = [
             f"ytdlleech{CMD_SUFFIX}",
-            f"ytl{CMD_SUFFIX}",
+            f"yl{CMD_SUFFIX}",
         ]
         self.CloneCommand = f"clone{CMD_SUFFIX}"
         self.CountCommand = f"count{CMD_SUFFIX}"
